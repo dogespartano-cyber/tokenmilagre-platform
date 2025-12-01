d2fe8b46fd8bb6f04dd1e162b5965fc480a6da77
Zen Foco <zenfoco@proton.me>
2025-12-01 18:19:18 -0300

    style(education): increase horizontal padding for article title and subtitle

--- tokenmilagre-platform/app/educacao/[slug]/ArtigoEducacionalClient.tsx
+++ tokenmilagre-platform/app/educacao/[slug]/ArtigoEducacionalClient.tsx
@@ -173,7 +173,7 @@ export default function ArtigoEducacionalClient({ article, relatedArticles = [] 
             }}
           />
 
-          <div className="container mx-auto px-4 relative z-10">
+          <div className="container mx-auto px-6 md:px-10 relative z-10">
             <div className="max-w-6xl">
 
 
